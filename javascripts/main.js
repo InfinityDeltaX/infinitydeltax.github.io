1cf6cfd1c5852b22b41538b9cdd0e361d65b7041
InfinityDeltaX <InfinityDeltaX@users.noreply.github.com>
2015-03-08 16:49:20 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+Console.log("test");
+var array = ["hello", "world", "fire"];
+
+for(var e in array) { // e = 0, 1, 2
+	console.log(array[e]);
+}
+
+var object = {"lexington": 44.7, "Belmont": 40.0, "Cambridge": 27};
+
+for(var town in object) {
+  console.log(town);
+}
